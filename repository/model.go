a7726520c0f2f89db34393d473a41219e3217bfc
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-07 15:09:34 +0200

    add repository and migration

---
+++ repository/model.go
@@ -0,0 +1,129 @@
+package repository
+
+import (
+	"time"
+)
+
+type (
+	MediaType     string
+	VideoCodec    string
+	AudioCodec    string
+	SubtitleCodec string
+)
+
+const (
+	MediaTypeMovie   MediaType = "Movie"
+	MediaTypeTvShow  MediaType = "TvShow"
+	MediaTypeEpisode MediaType = "Episode"
+
+	VideoCodecH264 VideoCodec = "H264"
+	VideoCodecH265 VideoCodec = "H265"
+	VideoCodecHEVC VideoCodec = "HEVC"
+
+	AudioCodecAAC    AudioCodec = "AAC"
+	AudioCodecAC3    AudioCodec = "AC3"
+	AudioCodecEAC3   AudioCodec = "EAC3"
+	AudioCodecMP3    AudioCodec = "MP3"
+	AudioCodecDTS    AudioCodec = "DTS"
+	AudioCodecVorbis AudioCodec = "Vorbis"
+
+	SubtitleCodecSRT    SubtitleCodec = "SRT"
+	SubtitleCodecSUBRIP SubtitleCodec = "SUBRIP"
+	SubtitleCodecASS    SubtitleCodec = "ASS"
+)
+
+type Model struct {
+	ID        string `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	//DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+type MediaFile struct {
+	Model
+	Filename  string
+	Size      float64
+	Duration  float64
+	Codec     VideoCodec
+	Mimetype  string
+	Audio     []Audio    `gorm:"foreignKey:MediaFileID;constraint:OnDelete:CASCADE;"`
+	Subtitles []Subtitle `gorm:"foreignKey:MediaFileID;constraint:OnDelete:CASCADE;"`
+}
+
+type Media struct {
+	Model
+	MediaType   MediaType  `gorm:"index"`
+	TmdbID      int        `gorm:"uniqueIndex"`
+	ReleaseDate time.Time  `gorm:"type:date"`
+	TvShows     []TvShow   `gorm:"foreignKey:MediaID;constraint:OnDelete:CASCADE;"`
+	Movies      []Movie    `gorm:"foreignKey:MediaID;constraint:OnDelete:CASCADE;"`
+	Episodes    []Episode  `gorm:"foreignKey:MediaID;constraint:OnDelete:CASCADE;"`
+	Categories  []Category `gorm:"many2many:category_media;constraint:OnDelete:CASCADE;"`
+}
+
+type TvShow struct {
+	Model
+	Name     string
+	MediaID  string    `gorm:"type:uuid;not null"`
+	Media    Media     `gorm:"reference:MediaID"`
+	Episodes []Episode `gorm:"foreignKey:TvShowID;constraint:OnDelete:CASCADE;"`
+}
+
+type Episode struct {
+	Model
+	Name        string
+	NbEpisode   int
+	NbSeason    int
+	MediaID     string    `gorm:"type:uuid;not null"`
+	Media       Media     `gorm:"reference:MediaID"`
+	TvShowID    string    `gorm:"type:uuid;not null"`
+	TvShow      TvShow    `gorm:"reference:TvShowID"`
+	MediaFileID string    `gorm:"type:uuid;not null"`
+	MediaFile   MediaFile `gorm:"reference:MediaFileID;constraint:OnDelete:CASCADE;"`
+}
+
+type Movie struct {
+	Model
+	Name        string
+	MediaID     string    `gorm:"type:uuid;not null"`
+	Media       Media     `gorm:"reference:MediaID"`
+	MediaFileID string    `gorm:"type:uuid;not null"`
+	MediaFile   MediaFile `gorm:"reference:MediaFileID;constraint:OnDelete:CASCADE;"`
+}
+
+type Audio struct {
+	Model
+	Codec       AudioCodec
+	Language    string
+	Bitrate     float64
+	MediaFileID string    `gorm:"type:uuid;not null"`
+	MediaFile   MediaFile `gorm:"reference:MediaFileID"`
+}
+
+type Subtitle struct {
+	Model
+	Codec       SubtitleCodec
+	Language    string
+	MediaFileID string    `gorm:"type:uuid;not null"`
+	MediaFile   MediaFile `gorm:"reference:MediaFileID"`
+}
+
+type Category struct {
+	Model
+	Name   string  `gorm:"uniqueIndex"`
+	Medias []Media `gorm:"many2many:category_media"`
+}
+
+type CategoryMedia struct {
+	MediaID    string   `gorm:"type:uuid;primaryKey"`
+	Media      Media    `gorm:"reference:MediaID;constraint:OnDelete:CASCADE;"`
+	CategoryID string   `gorm:"type:uuid;primaryKey"`
+	Category   Category `gorm:"reference:CategoryID;constraint:OnDelete:CASCADE;"`
+}
+
+type Rating struct {
+	UserID  string `gorm:"type:uuid;primaryKey"`
+	MediaID string `gorm:"type:uuid;primaryKey"`
+	Media   Media  `gorm:"reference:MediaID;constraint:OnDelete:CASCADE;"`
+	Rating  int
+}
---
+++ repository/root.go
@@ -0,0 +1,20 @@
+package repository
+
+import (
+	"gorm.io/gorm"
+)
+
+func Migrate(db *gorm.DB) error {
+	return db.AutoMigrate(
+		&MediaFile{},
+		&Media{},
+		&TvShow{},
+		&Episode{},
+		&Movie{},
+		&Audio{},
+		&Subtitle{},
+		&Category{},
+		&CategoryMedia{},
+		&Rating{},
+	)
+}
